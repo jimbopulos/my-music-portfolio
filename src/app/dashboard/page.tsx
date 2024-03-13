105594d38ae5ac66b8bfe1d51a3a75e5b40416e7
jimbopulos <james.mgalantino@gmail.com>
2024-03-13 13:26:57 -0400

    Add dashboard, footer component, dir restructure

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Footer } from '@/components';
+
+export default function Dashboard() {
+  return (
+    <main className="flex flex-col min-h-screen items-center">
+      <nav>this is a navbar</nav>
+      <div className="h-full">this is the home page...</div>
+      <Footer />
+    </main>
+  );
+}
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="flex flex-col items-center">
+      <p>I am a footer</p>
+    </footer>
+  );
+}
